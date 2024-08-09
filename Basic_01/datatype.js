3c54a3173b18a4917eab1250ce6f11ffe4cd00a9
qasimch01 <48183636+qasimch01@users.noreply.github.com>
2024-08-09 17:53:00 +0000

    in this we discus some data types

---
+++ Basic_01/datatype.js
@@ -0,0 +1,10 @@
+// we have diffrent type of data type but we usly use the inter , string boolean .
+let name ="Qasim Ch "   //this is string data type 
+let age = 28   //this is intager data type
+let year = 2.5  //its look like int but this is floting value 
+let night = true //this is boolean data type we just use in true or false.
+let hi = null;  // this is null value when we console this we see this is object 
+let we = undefined; // when we run they show us this is undefined.
+
+console.table([name , age, year,night,hi,we])
+console.table(typeof name )  // in this we can check which of your data 
