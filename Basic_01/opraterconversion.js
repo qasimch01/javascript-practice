98bb9698aec43aaa765a510449a434fa06035d0c
qasimch01 <48183636+qasimch01@users.noreply.github.com>
2024-08-10 06:18:14 +0000

    in this we will prectice oprater

---
+++ Basic_01/opraterconversion.js
@@ -0,0 +1,10 @@
+let num1 = 80
+let num2 = 70
+let ans = num1 + num2  //like this we can create calculater they calculate the value + / _ * and ect
+let ans1 = num1 > num2 // this is comparision oprater like less then grater then equal to etc
+
+  
+console.log( "sum of this number " + ans);
+console.log( "this number " + ans1);
+
+
