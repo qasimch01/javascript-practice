51ff01fcc099e6ec3b999c3f7f14d35dc3d05114
qasimch01 <48183636+qasimch01@users.noreply.github.com>
2024-08-13 11:24:06 +0000

    now we learn and prectice function with obj aryy

---
+++ Basic_03/functios.js
@@ -0,0 +1,33 @@
+//function syntex 
+function myfun(){
+    console.log("qas");
+    console.log("assa");
+    console.log("assa");
+    console.log("assa");
+    
+}
+//now we can call our function
+myfun()  // this is simple method to call function give name and key.
+
+// function addd(num1,num2){
+
+//     console.log(num1+num2);
+    
+// }
+// addd(5,9)
+
+function addd(num1,num2){
+
+    let result = num1+num2
+return result    
+}
+ const result = addd(3,5)
+ console.log("my result",result);
+ //pass rust oprater in function
+ function ad(...numm){
+    return numm
+
+ }
+
+ console.log((ad(10,10,20,)));
+ 
